c98fecc3532c15287304a19517dd11fb8c5ff010
kazuoikeda1411 <erizabesu1411@icloud.com>
2024-01-21 17:40:51 +0900

    ユーザー一覧処理の追加

--- app/controller/controller.go
+++ app/controller/controller.go
@@ -6,30 +6,30 @@ import(
 	"github.com/gin-gonic/gin"
 )
 
-type User struct{
-	Id int `json:"id`
-	Name string `json:"name`
-}
+// type User struct{
+// 	Id int `json:"id`
+// 	Name string `json:"name`
+// }
 
 func Index(c *gin.Context){
 	users := model.GetAll()
 	c.HTML(200, "index.html", gin.H{"users": users})
 }
 
-func GetUser(c *gin.Context){
-	user := User{
-		Id: 1,
-		Name: "タロウ",
-	}
-	c.IndentedJSON(200, user)
-}
+// func GetUser(c *gin.Context){
+// 	user := User{
+// 		Id: 1,
+// 		Name: "タロウ",
+// 	}
+// 	c.IndentedJSON(200, user)
+// }
 
-func PostCreate(c *gin.Context) {
-	name := c.PostForm("name")
-	email := c.PostForm("email")
-	password := c.PostForm("password")
-	user := model.User{Name: name, Email: email, Password: password}
-	user.Create()
-	c.IndentedJSON(200, user)
-	// c.Redirect(301, "/")
-}
+// func PostCreate(c *gin.Context) {
+// 	name := c.PostForm("name")
+// 	email := c.PostForm("email")
+// 	password := c.PostForm("password")
+// 	user := model.User{Name: name, Email: email, Password: password}
+// 	user.Create()
+// 	c.IndentedJSON(200, user)
+// 	// c.Redirect(301, "/")
+// }
--- app/model/users.go
+++ app/model/users.go
@@ -1,14 +1,23 @@
 package model
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type User struct {
-	gorm.Model
+	gorm.Model `json:"-"`
+	UserId int
 	Name string
 	Email string
 	Password string
+	UserIcon string
+	Role string
+	PasswordResetToken string
+	CreatedAt time.Time
+    UpdatedAt time.Time
+    DeletedAt gorm.DeletedAt
 }
 
 func GetAll() (users []User) {
@@ -19,11 +28,18 @@ func GetAll() (users []User) {
 	return
 }
 
-func (u *User) Create() {
-	result := Db.Create(u)
+// func (u *User) Create() {
+// 	result := Db.Create(u)
+// 	if result.Error != nil {
+// 		panic(result.Error)
+// 	}
+// 	return
+// }
+
+func GetUsers()(users []User){
+	result := Db.Where("deleted_at IS NULL").Find(&users)
 	if result.Error != nil {
 		panic(result.Error)
 	}
 	return
 }
-
