a320d0c5b63bc974ff5f1fc605a92987845e55c2
kazuoikeda1411 <erizabesu1411@icloud.com>
2024-01-23 22:42:17 +0900

    バリデーションの実装

--- app/controller/admin_user_controller.go
+++ app/controller/admin_user_controller.go
@@ -1,23 +1,46 @@
 package controller
 
 import (
-	"net/http"
 	"app/service"
+	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator"
 )
 
 type User struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	Role  string `json:"role"`
+	Name  string `json:"name" validate:"required,gte=0,lte=100"`
+	Email string `json:"email" validate:"required,email"`
+	Role  string `json:"role" validate:"oneof=admin user"`
 }
+
+var validate *validator.Validate
 
 func CreateUsers(c *gin.Context) {
 	var user User
 	if err := c.ShouldBindJSON(&user); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
+	}
+
+	validate = validator.New()
+	validateErr := validate.Struct(user)
+	if validateErr!=nil{
+		for _, err := range validateErr.(validator.ValidationErrors) {
+			fmt.Println(err.Namespace())
+			fmt.Println(err.Field())
+			fmt.Println(err.StructNamespace())
+			fmt.Println(err.StructField())
+			fmt.Println(err.Tag())
+			fmt.Println(err.ActualTag())
+			fmt.Println(err.Kind())
+			fmt.Println(err.Type())
+			fmt.Println(err.Value())
+			fmt.Println(err.Param())
+			fmt.Println()
+		}
+		c.IndentedJSON(400, user)
 	}
 	service.CreateUsers(user.Name,user.Email,user.Role)
 	c.IndentedJSON(201, user)
--- app/database/init/init_database.sql
+++ app/database/init/init_database.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS users(
     user_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     user_icon VARCHAR(255),
     role VARCHAR(255) NOT NULL,
