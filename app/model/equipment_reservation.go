e27960912e660931db06f33d1c4a426c82765773
karinana <naoki.kariya@rakus-partners.co.jp>
2024-03-17 12:02:37 +0900

    予約情報に機材とユーザー情報を同時に取得

--- app/model/equipment_reservation.go
+++ app/model/equipment_reservation.go
@@ -25,8 +25,14 @@ type WithUser struct {
 	UserIcon string
 }
 
+//機材情報の一部
+type WithEquipment struct {
+	EquipmentId int `gorm:"primaryKey;autoIncrement"`
+	Name string 
+}
+
 //機材予約の構造体
-type EquipmentReservationWithUser struct {
+type EquipmentReservationWithUserAndEquipment struct {
 	EquipmentReservationId uint `gorm:"primaryKey;autoIncrement" ,json:"equipmentReservationId,omitempty"`
 	UserId int `json:"userId,omitempty"`
 	EquipmentId int `json:"equipmentId,omitempty"`
@@ -35,17 +41,21 @@ type EquipmentReservationWithUser struct {
 	ActivityStartTime time.Time `json:"activityStartTime,omitempty"`
 	ActivityEndTime time.Time `json:"activityEndTime,omitempty"`
 	DeletedAt time.Time `json:"deletedAt,omitempty"`
+	Equipment WithEquipment `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;",json:"Equipment,omitempty"`
 	User WithUser `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;",json:"User,omitempty"`
 }
 
+var equipment Equipment
 var equipmentReservations []EquipmentReservation 
-var equipmentReservationsWithUser []EquipmentReservationWithUser
+var equipmentReservationsWithUserAndEquipment []EquipmentReservationWithUserAndEquipment
 
 //特定の機材に関する予約情報一覧を取得
-func GetEquipmentReservationsByEquipmentId(equipmentId int)(equipmentReservationsWithUser []EquipmentReservationWithUser){
+func GetEquipmentReservationsByEquipmentId(equipmentId int)(equipmentReservationsWithUserAndEquipment []EquipmentReservationWithUserAndEquipment){
 	result := Db.Debug().Model(&EquipmentReservation{}).Preload("User", func(Db *gorm.DB) *gorm.DB {
-		return Db.Model(&User{})// 見に行くテーブルはusersであると明示
-	}).Where("equipment_id = ?",equipmentId).Where("deleted_at is null").Where("reservation_end_time > ?",time.Now()).Order("reservation_start_time asc").Find(&equipmentReservationsWithUser)
+			return Db.Model(&User{})// 見に行くテーブルはusersであると明示
+		}).Preload("Equipment", func(Db *gorm.DB)*gorm.DB{
+			return Db.Table("equipments").Find(&Equipment{})
+	}).Where("equipment_id = ?",equipmentId).Where("deleted_at is null").Where("reservation_end_time > ?",time.Now()).Order("reservation_start_time asc").Find(&equipmentReservationsWithUserAndEquipment)
 	
 	if result.Error != nil {
 		panic(result.Error)
@@ -54,10 +64,12 @@ func GetEquipmentReservationsByEquipmentId(equipmentId int)(equipmentReservation
 }
 
 // 特定のユーザーが予約をしている情報一覧を取得
-func GetEquipmentReservationsByUserId(userId int)(equipmentReservationsWithUser []EquipmentReservationWithUser){
+func GetEquipmentReservationsByUserId(userId int)(equipmentReservationsWithUserAndEquipment []EquipmentReservationWithUserAndEquipment){
 	result := Db.Debug().Model(&EquipmentReservation{}).Preload("User", func(Db *gorm.DB) *gorm.DB {
 				return Db.Model(&User{})// 見に行くテーブルはusersであると明示
-	}).Where("user_id = ?",userId).Where("deleted_at is null").Where("reservation_end_time > ?",time.Now()).Order("reservation_start_time asc").Find(&equipmentReservationsWithUser)
+			}).Preload("Equipment", func(Db *gorm.DB) *gorm.DB {
+				return Db.Table("equipments").Find(&Equipment{})// 見に行くテーブルはequipmentであると明示
+			}).Where("user_id = ?",userId).Where("deleted_at is null").Where("reservation_end_time > ?",time.Now()).Order("reservation_start_time asc").Find(&equipmentReservationsWithUserAndEquipment)
 	if result.Error != nil {
 		panic(result.Error)
 	}
--- app/service/equipment_reservation_service .go
+++ app/service/equipment_reservation_service .go
@@ -4,12 +4,12 @@ import(
 	"app/model"
 )
 /*特定の機材の機材予約一覧を取得する*/
-func GetEquipmentReservationsByEquipmentId(equipmentId int) []model.EquipmentReservationWithUser{
+func GetEquipmentReservationsByEquipmentId(equipmentId int) []model.EquipmentReservationWithUserAndEquipment{
 	equipmentReservations := model.GetEquipmentReservationsByEquipmentId(equipmentId)
 	return equipmentReservations
 }
 /*特定のユーザーの機材予約一覧を取得する*/
-func GetEquipmentReservationsByUserId(userId int) []model.EquipmentReservationWithUser{
+func GetEquipmentReservationsByUserId(userId int) []model.EquipmentReservationWithUserAndEquipment{
 	equipmentReservations := model.GetEquipmentReservationsByUserId(userId)
 	return equipmentReservations
 }
